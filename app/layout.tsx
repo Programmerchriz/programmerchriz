9a7846fbc01713072aa50473275cf8744efe5f51
ProgrammerChris6002 <ProgrammerChris6002@gmail.com>
2026-04-16 15:59:54 +0100

    Added ScrollToTop functionality

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import ReponsiveNav from "@/components/Navbar/ReponsiveNav";
+import ScrollToTop from "@/components/Helper/ScrollToTop";
 
 const fonts = Inter({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
@@ -78,6 +79,7 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col overflow-x-hidden">
         <ReponsiveNav />
         {children}
+        <ScrollToTop />
       </body>
     </html>
   );
